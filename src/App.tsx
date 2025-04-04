37b8380ee37af21bf22665954176367d8a89657b
Utkarsh Agrawal <2837124a@student.gla.ac.uk>
2025-04-04 12:02:35 +0200

    Phone Number input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
+  // Store Phone number
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [error, setError] = useState('');
+
+  // Function that handle input change
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const isValidPhoneNumber = (phone: string) => {
+    const phoneRegex = /^\+\d{1,3}\d{10}$/;
+    return phoneRegex.test(phone);
+  };
+  // Function to submit phone number
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (isValidPhoneNumber(phoneNumber)) {
+      alert(`Phone number submitted: ${phoneNumber}`);
+      setPhoneNumber(''); // Clear input field after submission
+      setError('');
+    } else {
+      setError('Invalid phone number. Format: +[Country Code][10-digit number]');
+    } // Clear input field after submission
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+        
+      <h1>Enter Your Phone Number</h1>
+        <form onSubmit={handleSubmit}>
+          <Form.Control isInvalid={!!error}
+            type="tel" 
+            placeholder="+123456789012" 
+            value={phoneNumber} 
+            onChange={handleInputChange} 
+            required 
+          />
+          {error && <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>}
+          
+          <Button type="submit">Submit</Button>
+
+        </form>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
+ 
+
+
+
+
+
+
+
