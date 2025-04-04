bd76aecbf0775c600175e341819d3c9c7f7c554b
Utkarsh Agrawal <2837124a@student.gla.ac.uk>
2025-04-04 13:40:14 +0200

    Showing Status of Employee on Admin Page

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Container, Form } from 'react-bootstrap';
+import { User } from '../models/User';
+
+
+const usersData: User[] = [
+  { id: "1", name: "Captain Giggles", phone: "+12345678901", working: true },
+  { id: "2", name: "Sir Laughs-a-Lot", phone: "+12345678902", working: false },
+  { id: "3", name: "Duke Chuckles", phone: "+12345678903", working: true },
+  { id: "4", name: "Lady Snort", phone: "+12345678904", working: false }
+];
 
 export default function AdminPage() {
-    return (
-        <Container>AdminPage</Container>
-    )
+  const [users] = useState<User[]>(usersData);
+  const handleSwitchChange = (userId: string) => {
+    // Toggle the "working" status of the user
+    // const updatedUsers = users.map(user => {
+    //   if (user.id === userId) {
+    //     return { ...user, working: !user.working };
+    //   }
+    //   return user;
+    // });
+    //setUsers(updatedUsers);
+  };
+
+  return (
+    <Container>
+      <h1>Admin Page - Registered Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.phone}
+            <Form.Check
+                type="switch"
+                id={`switch-${user.id}`}
+              //  label={user.working ? "Working" : "Not Working"}
+                checked={user.working}
+                onChange={() => handleSwitchChange(user.id)}
+              />
+            
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
 }
+
+
+
+
+
+
+
+
